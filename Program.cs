2ade225389e5cf3c8f9b7ba215c56eee6033abd6
Egor Grigoryev <e.grigoryev@egrigoryev.local>
2018-10-03 12:54:57 +0300

    попытка прекратить падения вечным пеерзапуском

--- Program.cs
+++ Program.cs
@@ -10,12 +10,12 @@ namespace VkLikeSiteBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Start()
         {
             BotSettings settings = BotSettings.GetSettings();
             List<Bot> bots = new List<Bot>();
 
-            for(int i = 0; i < settings.Users.Length; i++)
+            for (int i = 0; i < settings.Users.Length; i++)
             {
                 SiteUserContext user = settings.Users[i];
 
@@ -26,18 +26,33 @@ namespace VkLikeSiteBot
 
                     Console.WriteLine($"{user.Login} {user.Pass} authorized\n");
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     Console.WriteLine($"{user.Login} {user.Pass} not authorized: {ex.Message}");
                 }
             }
 
             Task[] botsWorkCycles = new Task[bots.Count];
-            for(int i = 0; i < bots.Count; i++)
+            for (int i = 0; i < bots.Count; i++)
                 botsWorkCycles[i] = bots[i].StartAsync();
 
             Task.WaitAll(botsWorkCycles);
             Console.WriteLine("all bots stoped working");
+        }
+
+        //временный костыль, я пока не понял с чем были связанны периобические выледы при повторной авторизации
+        //скорее всего на серверы просто была старая версия, но не стоит исключать, что ошибка где-то тут
+        static void Main(string[] args)
+        {
+            try
+            {
+                Start();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"{DateTime.UtcNow}\n{ex.Message}");
+                Main(null);
+            }
         }
     }
 }
